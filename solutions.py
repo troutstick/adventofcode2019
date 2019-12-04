22855af3624907d3e1b59f784aca988491ccf42b
troutstick <54828054+troutstick@users.noreply.github.com>
2019-12-04 14:53:17 -0800

    day2 part1 complete

--- solutions.py
+++ solutions.py
@@ -2,9 +2,9 @@
 # Day 1
 #####################
 
-def day1_part1():
+def day1_p1():
     """
-    >>> day1_part1()
+    >>> day1_p1()
     3334297
     """
     ans = 0
@@ -14,15 +14,17 @@ def day1_part1():
             ans += (mass // 3) - 2
     return ans
 
-def fuel_calculator(mass):
-    """Recursively calculates fuel requirement as based on Day 2 puzzle"""
-    fuel_needed = (mass // 3) - 2
-    if fuel_needed < 0:
-        return 0
-    else:
-        return fuel_needed + fuel_calculator(fuel_needed)
 
-def day1_part2():
+def day1_p2():
+
+    def fuel_calculator(mass):
+        """Recursively calculates fuel requirement as based on Day 2 puzzle"""
+        fuel_needed = (mass // 3) - 2
+        if fuel_needed < 0:
+            return 0
+        else:
+            return fuel_needed + fuel_calculator(fuel_needed)
+
     ans = 0
     with open("day1input.txt", "r") as file:
         for line in file:
@@ -35,5 +37,49 @@ def day1_part2():
 # Day 2
 #####################
 
-def day2_part1():
+from operator import add, mul
+
+def intcode(input_list):
+    """Takes in an input list and mutates it accordingly"""
+
+    def intcode_helper(index):
+        """Runs one iteration of the intcode program"""
+        opcode = input_list[index]
+        if opcode == 1:
+            operator = add
+        elif opcode == 2:
+            operator = mul
+        elif opcode == 99:
+            raise IntcodeException
+        else:
+            print("you shouldn't see me")
+
+        first_index = input_list[index+1]
+        second_index = input_list[index+2]
+        output_index = input_list[index+3]
+
+        input_list[output_index] = operator(input_list[first_index], input_list[second_index])
+
+    i = 0
+    while i < (len(input_list) - 1):
+        try:
+            intcode_helper(i)
+            i += 4
+        except IntcodeException:
+            break
+
+def day2_p1():
     with open("day2input.txt", "r") as file:
+        contents = file.read()
+        input_list_str = contents.split(",")
+        input_list = []
+        for str in input_list_str:
+            input_list.append(int(str))
+        # the list is now initialized!
+    input_list[1] = 12
+    input_list[2] = 2
+    intcode(input_list)
+    return input_list[0]
+
+class IntcodeException(Exception):
+    pass
